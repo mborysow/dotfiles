218369cbe20b67c5e117f7c30525dddba8855e4b
Michael Borysow <m@borysow.com>
2026-02-14 12:49:00 -0600

    converted to zshrc.include to a template

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -1,22 +1,22 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/borysow/.oh-my-zsh/custom/plugins/fzf/bin* ]]; then
-  export PATH="$PATH:/Users/borysow/.oh-my-zsh/custom/plugins/fzf/bin"
+if [[ ! "$PATH" == *$HOME/.oh-my-zsh/custom/plugins/fzf/bin* ]]; then
+  export PATH="$PATH:$HOME/.oh-my-zsh/custom/plugins/fzf/bin"
 fi
 
 # Man path
 # --------
-if [[ ! "$MANPATH" == */Users/borysow/.oh-my-zsh/custom/plugins/fzf/man* && -d "/Users/borysow/.oh-my-zsh/custom/plugins/fzf/man" ]]; then
-  export MANPATH="$MANPATH:/Users/borysow/.oh-my-zsh/custom/plugins/fzf/man"
+if [[ ! "$MANPATH" == *$HOME/.oh-my-zsh/custom/plugins/fzf/man* && -d "$HOME/.oh-my-zsh/custom/plugins/fzf/man" ]]; then
+  export MANPATH="$MANPATH:$HOME/.oh-my-zsh/custom/plugins/fzf/man"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/borysow/.oh-my-zsh/custom/plugins/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.oh-my-zsh/custom/plugins/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/borysow/.oh-my-zsh/custom/plugins/fzf/shell/key-bindings.zsh"
+source "$HOME/.oh-my-zsh/custom/plugins/fzf/shell/key-bindings.zsh"
 
 bindkey -s '^p' 'vim $(fzf)\n'
 
